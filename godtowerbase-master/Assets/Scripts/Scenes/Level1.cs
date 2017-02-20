b384135aa9b413781f8cacfe17a8a37644773621
cuong.pham.1606 <phamphucuong1606@gmail.com>
2017-02-20 18:32:52 +0700

    update 20/2

---
+++ godtowerbase-master/Assets/Scripts/Scenes/Level1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level1 : MonoBehaviour {
+    public InputField passwordInput;
+    public Text hintTextField;
+
+    public void OnSubmitButtonClicked()
+    {
+        if(passwordInput.text == "kabul")
+        {
+            Application.LoadLevel("StartScene");
+        }
+        else
+        {
+            hintTextField.color = Color.red;
+            hintTextField.text = "ACCESS DENIED!!!";
+        }
+    } 
+}
---
+++ godtowerbase-master/Assets/Scripts/Scenes/StartScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScene : MonoBehaviour {
+
+	public void OnStartButtonClicked(){
+        TKSceneManager.ChangeScene("Level1");
+    }
+
+    public void OnContinueButtonClicked() {
+
+    }
+
+}
